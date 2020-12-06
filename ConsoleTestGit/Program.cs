cef22fe0c5d5aeb0d1386dc005a0baff67850c5f
Igor <gamehomm@gmail.com>
2020-12-06 21:37:29 +0400

    Add new answer

--- ConsoleTestGit/Program.cs
+++ ConsoleTestGit/Program.cs
@@ -5,11 +5,13 @@ namespace ConsoleTestGit
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+        {            
             Console.WriteLine("What is your name?");
             Console.WriteLine("I am Visual Studio!");
+            Console.Write("My name is ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            Console.ReadKey();
         }
     }
 }
