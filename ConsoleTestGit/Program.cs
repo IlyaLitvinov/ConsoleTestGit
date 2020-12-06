0bef3255f23e52c143adc8945d870bf09ed03bad
Igor <gamehomm@gmail.com>
2020-12-06 22:51:03 +0400

    Add some cycle

--- ConsoleTestGit/Program.cs
+++ ConsoleTestGit/Program.cs
@@ -8,11 +8,18 @@ namespace ConsoleTestGit
         {            
             Console.WriteLine("What is your name?");
             Console.WriteLine("I am Visual Studio!");
-            Console.Write("My name is ");
-            string name = Console.ReadLine();
-            var user = new User();
-            user.Name = name;
-            Console.WriteLine($"Hello, {user.Name}!");
+            while (true)
+            {
+                Console.Write("My name is ");
+                string name = Console.ReadLine();
+                if (name == "Выход" || name == "выход" || name == "exit" || name == "Exit")
+                    break;
+
+                var user = new User();
+                user.Name = name;
+                Console.WriteLine($"Hello, {user.Name}!");
+            }
+            Console.WriteLine("Good bye!");
             Console.ReadKey();
         }
     }
